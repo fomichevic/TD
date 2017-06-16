710c31121c08d3c2c78e7b1cebf695935ebcf4c8
Anton Surkis <antonsurkis@gmail.com>
2017-06-16 15:39:46 +0300

    Changed usage sample

--- static/sample.js
+++ static/sample.js
@@ -1,24 +1,31 @@
-const rm = ResourceManager.get(); // Можно каждый раз использовать ResourceManager.get(), rm -- сокращение
-rm.onload = () => { // Передача асинхронная, нужен обработчик
-    const gm = {}; // Графического менеджера пока в этой ветке нет
-    const type = 'static'; // Тип графического объекта. 'static', 'dynamic' или 'background'
-    const id = 'id'; // Идентификатор объекта
+(function() { // Обернем все в анонимную функцию, чтобы этот код не был выполнен, а был только примером
 
-    const sprite = gm[type][id]; // Спрайт
+    const rm = ResourceManager.get(); // Можно каждый раз использовать ResourceManager.get(), rm -- сокращение
+    rm.onload = () => { // Передача асинхронная, нужен обработчик события
+        const gm = GraphicsManager.get(); // Графического менеджера пока в этой ветке нет, но обращение к нему будет таким
+        const type = 'static'; // Тип графического объекта. 'static', 'dynamic' или 'background'
+        const id = 'id'; // Идентификатор объекта
 
-    const texture = sprite.texture; // Супертекстура, используемая спрайтом
-    const descriptor = sprite.descriptor; // Дескриптор, используемый спрайтом
+        const sprite = gm[type][id]; // Спрайт
 
-    const img = rm.textures[texture].img; // Само изображение
-    const desc = rm.textures[texture].desc[descriptor]; // Дескриптор
-    const left   = desc.left;   // Левая граница изображения
-    const top    = desc.top;    // Верхняя граница изображения
-    const right  = desc.right;  // Правая граница изображения
-    const bottom = desc.bottom; // Нижняя граница изображения
+        const texture = sprite.texture; // Супертекстура, используемая спрайтом
+        const descriptor = sprite.descriptor; // Дескриптор, используемый спрайтом
 
-    const x = sprite.x; // Координата по оси X
-    const y = sprite.y; // Координата по оси Y
-    const w = sprite.w; // Размер по оси X
-    const h = sprite.h; // Размер по оси Y
-};
-rm.load('list'); // Ссылка на список с сервера
+        const img = rm.textures[texture].img; // Само изображение
+        const desc = rm.textures[texture].desc[descriptor]; // Дескриптор
+        const left = desc.left;   // Левая граница изображения
+        const top = desc.top;    // Верхняя граница изображения
+        const right = desc.right;  // Правая граница изображения
+        const bottom = desc.bottom; // Нижняя граница изображения
+
+        const pos = sprite.position; // Положение
+        const size = sprite.size; // Размер
+
+        const x = pos.x; // Координата по оси X
+        const y = pos.y; // Координата по оси Y
+        const w = size.x; // Размер по оси X
+        const h = size.y; // Размер по оси Y
+    };
+    rm.load('list'); // Начинаем скачивание. На месте 'list' -- путь до списка на сервере
+
+});
